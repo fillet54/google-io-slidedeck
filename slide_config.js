124c3c02bfbe85f9496cbf471acf30643c24f61c
Phillip Gomez <phillip.gomez@ngc.com>
2014-08-14 23:03:54 -0700

    Adding NGC Lunch & Learn template

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: "Title Goes Here",
+    subtitle: "subtile too!",
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -12,22 +12,22 @@ var SLIDE_CONFIG = {
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/square-logo-tiny.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
     ],
-    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+    theme: ['ngc_lunch_and_learn'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Phillip Gomez',
+    company: 'Developer<br>Northrop Grumman',
+    //gplus: 'dont have one :(',
+    twitter: '@phillipgomez',
+    www: 'http://www.philgomez.com',
+    github: 'http://github.com/fillet54'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
